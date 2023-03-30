6a48dd8a2fed77d13fa43d28ce7eb633fa769a8a
Juan Felipe <juanfelipealtran@gmail.com>
2023-03-30 08:55:34 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+function Banner(){
+
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="O banner principal"></img>
+        </header>
+    )
+}
+
+export default Banner
